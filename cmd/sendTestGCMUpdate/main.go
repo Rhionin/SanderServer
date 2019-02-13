b52e3c0f5db21e2ed8f3ed829f2433939b53b61b
CJ Campbell <cjc@qualtrics.com>
2019-02-12 21:44:33 -0700

    send test message entrypoint working on firebase

--- cmd/sendTestGCMUpdate/main.go
+++ cmd/sendTestGCMUpdate/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/Rhionin/SanderServer/firebase"
 	"github.com/Rhionin/SanderServer/progress"
 )
 
 func main() {
+	firebaseCredentialsConfigPath := os.Getenv("FIREBASE_CONFIG")
+	if len(firebaseCredentialsConfigPath) == 0 {
+		panic("Must provide a FIREBASE_CONFIG path")
+	}
+
+	ctx := context.Background()
+	firebaseClient, err := firebase.NewMessagingClient(ctx, firebaseCredentialsConfigPath)
+	if err != nil {
+		fmt.Println(err)
+		panic("Failed to initialize Firebase messaging client")
+	}
+
 	wips := []progress.WorkInProgress{
 		{Title: "Book 1", Progress: 25},
 		{Title: "Book 2 has a very long name copyedit and stuff", Progress: 50, PrevProgress: 30},
@@ -12,5 +29,10 @@ func main() {
 		{Title: "Book 4", Progress: 100, PrevProgress: 80},
 	}
 
-	progress.SendGCMUpdate(wips, "/topics/devprogress")
+	response, err := progress.SendFCMUpdate(ctx, firebaseClient, wips, "/topics/devprogress")
+	if err != nil {
+		fmt.Printf("Error sending FCM update: %s\n", err)
+	}
+
+	fmt.Println(response)
 }
--- progress/scheduledProgressUpdate.go
+++ progress/scheduledProgressUpdate.go
@@ -1,13 +1,10 @@
 package progress
 
 import (
-	"fmt"
+	"context"
+	"encoding/json"
 
-	"reflect"
-
-	"github.com/robfig/cron"
-	cfg "github.com/Rhionin/SanderServer/config"
-	"github.com/Rhionin/SanderServer/gcm"
+	"firebase.google.com/go/messaging"
 )
 
 type (
@@ -34,61 +31,68 @@ type (
 	}
 )
 
-// ScheduleProgressCheckJob schedules a job to repeatedly check progress
-// on Brandon Sanderson's books
-func (m *Monitor) ScheduleProgressCheckJob() {
-	prevWips := m.History.GetProgress()
+// // ScheduleProgressCheckJob schedules a job to repeatedly check progress
+// // on Brandon Sanderson's books
+// func (m *Monitor) ScheduleProgressCheckJob() {
+// 	prevWips := m.History.GetProgress()
 
-	config := cfg.GetConfig()
+// 	config := cfg.GetConfig()
 
-	c := cron.New()
-	fmt.Println(config.ProgressCheckInterval)
-	c.AddFunc(config.ProgressCheckInterval, func() {
-		currentWips := CheckProgress() // m.LiveReader.GetProgress()
-		if len(currentWips) > 0 {
-			m.History.WriteProgress(currentWips)
+// 	c := cron.New()
+// 	fmt.Println(config.ProgressCheckInterval)
+// 	c.AddFunc(config.ProgressCheckInterval, func() {
+// 		currentWips := CheckProgress() // m.LiveReader.GetProgress()
+// 		if len(currentWips) > 0 {
+// 			m.History.WriteProgress(currentWips)
 
-			if len(prevWips) > 0 {
-				areEqual := reflect.DeepEqual(currentWips, prevWips)
+// 			if len(prevWips) > 0 {
+// 				areEqual := reflect.DeepEqual(currentWips, prevWips)
 
-				if !areEqual {
-					fmt.Println("Update found! Pushing notification. Next check at", c.Entries()[0].Next)
+// 				if !areEqual {
+// 					fmt.Println("Update found! Pushing notification. Next check at", c.Entries()[0].Next)
 
-					// Get previous progress for existing works in progress
-					wipsUpdate := make([]WorkInProgress, len(currentWips))
-					copy(wipsUpdate, currentWips)
-					for i := 0; i < len(wipsUpdate); i++ {
-						currentWip := &wipsUpdate[i]
-						for j := 0; j < len(prevWips); j++ {
-							prevWip := &prevWips[j]
-							if currentWip.Title == prevWip.Title && currentWip.Progress != prevWip.Progress {
-								currentWip.PrevProgress = prevWip.Progress
-							}
-						}
-					}
+// 					// Get previous progress for existing works in progress
+// 					wipsUpdate := make([]WorkInProgress, len(currentWips))
+// 					copy(wipsUpdate, currentWips)
+// 					for i := 0; i < len(wipsUpdate); i++ {
+// 						currentWip := &wipsUpdate[i]
+// 						for j := 0; j < len(prevWips); j++ {
+// 							prevWip := &prevWips[j]
+// 							if currentWip.Title == prevWip.Title && currentWip.Progress != prevWip.Progress {
+// 								currentWip.PrevProgress = prevWip.Progress
+// 							}
+// 						}
+// 					}
 
-					SendGCMUpdate(wipsUpdate, "/topics/progress")
-				} else {
-					fmt.Println("No update. Next check at", c.Entries()[0].Next)
-				}
-			}
-		}
-		prevWips = currentWips
-	})
-	fmt.Println("First check at", c.Entries()[0].Next)
-	c.Start()
+// 					SendFCMUpdate(wipsUpdate, "/topics/progress")
+// 				} else {
+// 					fmt.Println("No update. Next check at", c.Entries()[0].Next)
+// 				}
+// 			}
+// 		}
+// 		prevWips = currentWips
+// 	})
+// 	fmt.Println("First check at", c.Entries()[0].Next)
+// 	c.Start()
 
-}
+// }
 
-// SendGCMUpdate pushes an update via GCM
-func SendGCMUpdate(wips []WorkInProgress, recipient string) {
+// SendFCMUpdate pushes an update via FCM
+func SendFCMUpdate(ctx context.Context, firebaseClient *messaging.Client, wips []WorkInProgress, topic string) (string, error) {
 
-	message := gcm.Message{
-		To: recipient,
-		Data: map[string]interface{}{
-			"worksInProgress": wips,
+	wipsStr, err := json.Marshal(wips)
+	if err != nil {
+		return "", err
+	}
+
+	message := &messaging.Message{
+		Topic: topic,
+		Android: &messaging.AndroidConfig{
+			Data: map[string]string{
+				"worksInProgress": string(wipsStr),
+			},
 		},
 	}
 
-	gcm.Send(message)
+	return firebaseClient.Send(ctx, message)
 }
